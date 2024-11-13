95482c5fe97855f6a24a2e9cf944c222eb6f9df4
Lev <101640837+DanteR03@users.noreply.github.com.>
2024-11-14 02:22:34 +0500

    Add buttons to clear the board and choose the size of the board

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const cellContainer = document.querySelector("#divs-container");
+const button1 = document.querySelector("#button-one");
+const button2 = document.querySelector("#button-two");
+button1.addEventListener("click", clearBoard);
+button2.addEventListener("click", () => {
+    let gridSize = prompt("Enter grid size(16 for 16x16, max 100)");
+    createGrid(gridSize);
+})
 
 function colorCell(cellEvent) {
     cellEvent.target.classList.add("colored-cell");
 }
 
-function createGrid(size) {
+function clearBoard() {
     cellContainer.innerHTML = "";
+}
+
+function createGrid(size) {
+    clearBoard();
     // Container is square //
     let cellContainerWidth = 600;
     if (size <= 100) {
